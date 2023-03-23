580fc13d8eea2f0b47edd716988ff36aff3b8970
Unam <97320894+manukao@users.noreply.github.com>
2023-03-23 15:48:53 +0100

    consume own api rout to display its data in frontend

---
+++ pages/products/index.js
@@ -0,0 +1,23 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Products() {
+  const { data, error } = useSWR("/api/products", fetcher);
+
+  if (error) return <div>Failed to load products</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {data.map((product) => (
+          <li key={product.id}>
+            <a href={`/products/${product.id}`}>{product.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
